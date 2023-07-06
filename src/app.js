86500128675ba5d33ddde7b924c3b98db1f0b968
sofi-rv <128938425+sofi-rv@users.noreply.github.com>
2023-07-06 02:19:28 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "raccon"];
+let action = ["run", "dance"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let a = 0; a < pronoun.length; a++) {
+  for (let b = 0; b < adj.length; b++) {
+    for (let c = 0; c < noun.length; c++) {
+      for (let d = 0; d < action.length; d++) {
+        document.querySelector("#domain").innerHTML +=
+          "<p>" + pronoun[a] + adj[b] + noun[c] + action[d] + ".com</p>";
+      }
+    }
+  }
+}
